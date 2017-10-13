02a2b4b07987c20d58697c1d339fa8da5501d731
JayD1 <31660846+JayD1@users.noreply.github.com>
2017-10-13 14:36:37 +0530

    mergesort updated!

--- merge.c
+++ merge.c
@@ -5,15 +5,19 @@ void merge(int a1[102], int sz1, int a2[102], int sz2, int a3[102])
 	int i=0,j=0,k=0;
 	while((i<sz1)&&(j<sz2))
 	{
-		if(a1[i]<a2[j])
-			a3[i+j]=a1[i++];
-		if(a1[i]>=a2[j])
-			a3[i+j]=a2[j++];
-		while(i<sz1)
-			a3[i+j]=a1[i++];
-		while(j<sz2)
-			a3[i+j]=a2[j++];
+		if(a1[i]<a2[j]){
+			a3[i + j]=a1[i];
+			i++;}
+		if(a1[i]>=a2[j]){
+			a3[i + j]=a2[j];
+			j++;}
 	}
+	while(i<sz1){
+		a3[i + j]=a1[i];
+		i++;}
+	while(j<sz2){
+		a3[i + j]=a2[j];
+		j++;}
 }
 
 void mergesort(int arr[100], int sz)
